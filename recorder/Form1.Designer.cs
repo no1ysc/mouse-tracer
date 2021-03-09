138ccb4cb8de8da92e873498c13bd0db95778cc3
Lucas <lucas@forspacelab.com>
2021-03-10 02:16:06 +0900

    added capture feature

---
+++ recorder/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace recorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnRecording = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 384);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnRecording
+            // 
+            this.btnRecording.Location = new System.Drawing.Point(607, 25);
+            this.btnRecording.Name = "btnRecording";
+            this.btnRecording.Size = new System.Drawing.Size(75, 23);
+            this.btnRecording.TabIndex = 1;
+            this.btnRecording.Text = "REC";
+            this.btnRecording.UseVisualStyleBackColor = true;
+            this.btnRecording.Click += new System.EventHandler(this.btnRecording_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(713, 25);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop/Save";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnRecording);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnRecording;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ recorder/Form1.cs
@@ -0,0 +1,90 @@
+﻿using Accord.Video.FFMPEG;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace recorder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ~Form1()
+        {
+            if (vfw != null && vfw.IsOpen)
+            {
+                vfw.Flush();
+                vfw.Close();
+            }
+        }
+
+        private VideoFileWriter vfw;
+        Bitmap img = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+        Graphics g;
+        string savePath = @"d:\임시전송\a.mp4";
+        long tickCount = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tickCount++;
+            g = Graphics.FromImage(img);
+            g.CopyFromScreen(0, 0, 0, 0, img.Size);
+            pictureBox1.Image = img;
+
+            if (vfw != null && vfw.IsOpen) 
+            {
+                vfw.WriteVideoFrame(img);
+                if (tickCount % 1000 == 0)
+                {
+                    vfw.Flush();
+                }
+            }
+        }
+
+        private void btnRecording_Click(object sender, EventArgs e)
+        {
+            btnRecording.Enabled = false;
+            btnStop.Enabled = true;
+
+            vfw = new VideoFileWriter()
+            {
+                Width = Screen.PrimaryScreen.Bounds.Width,
+                Height = Screen.PrimaryScreen.Bounds.Height,
+                FrameRate = 30,
+                BitRate = 1200 * 1000,
+                VideoCodec = VideoCodec.Mpeg4
+            };
+
+            vfw.Open(savePath);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnRecording.Enabled = true;
+            btnStop.Enabled = false;
+
+            vfw.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnRecording.Enabled = true;
+            btnStop.Enabled = false;
+
+            timer1 = new Timer();
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+
+            timer1.Start();
+        }
+    }
+}
