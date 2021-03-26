66bc8613a2bbf7e658518d21e28e782b9da62841
Lucas <lucas@forspacelab.com>
2021-03-27 01:21:02 +0900

    녹화 잘됨
    플레이어 히트맵 그리는 중,,,,,,

---
+++ player/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace player
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += Window_Loaded;
+            this.mediaElement.MediaOpened += mediaElement_MediaOpened;
+            this.playButton.Click += playButton_Click;
+            this.pauseButton.Click += pauseButton_Click;
+            this.stopButton.Click += stopButton_Click;
+            this.restartButton.Click += restartButton_Click;
+            this.previousButton.Click += previousButton_Click;
+            this.nextButton.Click += nextButton_Click;
+            this.fasterButton.Click += fasterButton_Click;
+            this.slowerButton.Click += slowerButton_Click;
+            this.setPositionButton.Click += setPositionButton_Click;
+        }
+
+        
+        private DispatcherTimer dispatcherTimer;
+        private MouseDecorder mouseDecorder;
+        private Graphics g;
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.dispatcherTimer = new DispatcherTimer();
+            this.dispatcherTimer.Interval = TimeSpan.FromSeconds(0.1d);
+            this.dispatcherTimer.Tick += dispatcherTimer_Tick;
+            this.mediaElement.Stop();
+            this.mediaElement.Source = new Uri(@"C:\Users\JS\Documents\mouse-tracer\2021-03-26 15-01-17.mp4");
+            mouseDecorder = new MouseDecorder(@"C:\Users\JS\Documents\mouse-tracer\2021-03-26 15-01-17.mtr");
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            ShowPosition();
+            // 계산 후 업데이트.
+            this.heatmap.Source = mouseDecorder.GetHeatmap(mediaElement.Position);
+        }
+
+        private void ShowPosition()
+        {
+            this.positionScrollBar.Value = this.mediaElement.Position.TotalSeconds;
+            this.positionTextBox.Text = this.mediaElement.Position.TotalSeconds.ToString("0.0");
+        }
+
+        private void setPositionButton_Click(object sender, RoutedEventArgs e)
+        {
+            TimeSpan timespan = TimeSpan.FromSeconds(double.Parse(this.positionTextBox.Text));
+            this.mediaElement.Position = timespan;
+
+            ShowPosition();
+        }
+        private void mediaElement_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            this.positionScrollBar.Minimum = 0;
+            this.positionScrollBar.Maximum = this.mediaElement.NaturalDuration.TimeSpan.TotalSeconds;
+            this.positionScrollBar.Visibility = Visibility.Visible;
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.mediaElement.Play();
+
+            SetButtnsEnabled(true);
+        }
+
+        private void pauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.mediaElement.Pause();
+
+            SetButtnsEnabled(false);
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.mediaElement.Stop();
+
+            SetButtnsEnabled(false);
+
+            ShowPosition();
+        }
+
+        private void restartButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.mediaElement.Stop();
+            this.mediaElement.Play();
+
+            SetButtnsEnabled(true);
+        }
+
+        private void previousButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.mediaElement.Position -= TimeSpan.FromSeconds(10);
+
+            ShowPosition();
+        }
+
+        private void nextButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.mediaElement.Position += TimeSpan.FromSeconds(10);
+
+            ShowPosition();
+        }
+        private void fasterButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.mediaElement.SpeedRatio *= 1.5d;
+        }
+
+        private void slowerButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.mediaElement.SpeedRatio /= 1.5d;
+        }
+
+        private void SetButtnsEnabled(bool isPlaying)
+        {
+            if (isPlaying)
+            {
+                this.playButton.IsEnabled = false;
+                this.pauseButton.IsEnabled = true;
+                this.playButton.Opacity = 0.5d;
+                this.pauseButton.Opacity = 1.0d;
+            }
+            else
+            {
+                this.playButton.IsEnabled = true;
+                this.pauseButton.IsEnabled = false;
+                this.playButton.Opacity = 1.0d;
+                this.pauseButton.Opacity = 0.5d;
+            }
+            this.dispatcherTimer.IsEnabled = isPlaying;
+        }
+    }
+}
---
+++ player/MouseDecorder.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace player
+{
+    public enum MouseEvent
+    {
+        Click,
+        DoubleClick,
+        Wheel,
+        Move,
+    }
+    public struct MouseEventPoint
+    {
+        public MouseEventPoint(string strTimeSapn, string mouseEvent, string x, string y)
+        {
+            this.timeSpan = TimeSpan.Parse(strTimeSapn);
+            this.mouseEvent = (MouseEvent)Enum.Parse(typeof(MouseEvent), mouseEvent);
+            this.x = int.Parse(x);
+            this.y = int.Parse(y);
+        }
+        public TimeSpan timeSpan;
+        public MouseEvent mouseEvent;
+        public int x;
+        public int y;
+    }
+    public class MouseDecorder
+    {
+        private TimeSpan startTime = TimeSpan.Zero;
+        private List<MouseEventPoint> data = new List<MouseEventPoint>();
+        public MouseDecorder(string fileName)
+        {
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+            foreach (string line in lines)
+            {
+                string[] rows = Regex.Split(line, ", ");
+                data.Add(new MouseEventPoint(rows[0], rows[1], rows[2], rows[3]));
+            }
+        }
+
+        public void SetStartTime(TimeSpan timeSpan)
+        {
+            this.startTime = timeSpan;
+        }
+
+        internal BitmapImage GetHeatmap(TimeSpan position)
+        {
+            using (var memory = new System.IO.MemoryStream())
+            {
+                bmp.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                return bitmapImage;
+            }
+        }
+
+        private List<HeatPoint> HeatPoints = new List<HeatPoint>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create new memory bitmap the same size as the picture box
+            Bitmap bMap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            // Initialize random number generator
+            Random rRand = new Random();
+            // Loop variables
+            int iX;
+            int iY;
+            byte iIntense;
+            // Lets loop 500 times and create a random point each iteration
+            for (int i = 0; i < 500; i++)
+            {
+                // Pick random locations and intensity
+                iX = rRand.Next(0, 200);
+                iY = rRand.Next(0, 200);
+                iIntense = (byte)rRand.Next(0, 120);
+                // Add heat point to heat points list
+                HeatPoints.Add(new HeatPoint(iX, iY, iIntense));
+            }
+            // Call CreateIntensityMask, give it the memory bitmap, and use it's output to set the picture box image
+            pictureBox1.Image = CreateIntensityMask(bMap, HeatPoints);
+        }
+        private Bitmap CreateIntensityMask(Bitmap bSurface, List<HeatPoint> aHeatPoints)
+        {
+            // Create new graphics surface from memory bitmap
+            Graphics DrawSurface = Graphics.FromImage(bSurface);
+            // Set background color to white so that pixels can be correctly colorized
+            DrawSurface.Clear(System.Drawing.Color.White);
+            // Traverse heat point data and draw masks for each heat point
+            foreach (HeatPoint DataPoint in aHeatPoints)
+            {
+                // Render current heat point on draw surface
+                DrawHeatPoint(DrawSurface, DataPoint, 15);
+            }
+            return bSurface;
+        }
+        private void DrawHeatPoint(Graphics Canvas, HeatPoint HeatPoint, int Radius)
+        {
+            // Create points generic list of points to hold circumference points
+            List<Point> CircumferencePointsList = new List<Point>();
+            // Create an empty point to predefine the point struct used in the circumference loop
+            Point CircumferencePoint;
+            // Create an empty array that will be populated with points from the generic list
+            Point[] CircumferencePointsArray;
+            // Calculate ratio to scale byte intensity range from 0-255 to 0-1
+            float fRatio = 1F / Byte.MaxValue;
+            // Precalulate half of byte max value
+            byte bHalf = Byte.MaxValue / 2;
+            // Flip intensity on it's center value from low-high to high-low
+            int iIntensity = (byte)(HeatPoint.Intensity - ((HeatPoint.Intensity - bHalf) * 2));
+            // Store scaled and flipped intensity value for use with gradient center location
+            float fIntensity = iIntensity * fRatio;
+            // Loop through all angles of a circle
+            // Define loop variable as a double to prevent casting in each iteration
+            // Iterate through loop on 10 degree deltas, this can change to improve performance
+            for (double i = 0; i <= 360; i += 10)
+            {
+                // Replace last iteration point with new empty point struct
+                CircumferencePoint = new Point();
+                // Plot new point on the circumference of a circle of the defined radius
+                // Using the point coordinates, radius, and angle
+                // Calculate the position of this iterations point on the circle
+                CircumferencePoint.X = Convert.ToInt32(HeatPoint.X + Radius * Math.Cos(ConvertDegreesToRadians(i)));
+                CircumferencePoint.Y = Convert.ToInt32(HeatPoint.Y + Radius * Math.Sin(ConvertDegreesToRadians(i)));
+                // Add newly plotted circumference point to generic point list
+                CircumferencePointsList.Add(CircumferencePoint);
+            }
+            // Populate empty points system array from generic points array list
+            // Do this to satisfy the datatype of the PathGradientBrush and FillPolygon methods
+            CircumferencePointsArray = CircumferencePointsList.ToArray();
+            // Create new PathGradientBrush to create a radial gradient using the circumference points
+            PathGradientBrush GradientShaper = new PathGradientBrush(CircumferencePointsArray);
+            // Create new color blend to tell the PathGradientBrush what colors to use and where to put them
+            ColorBlend GradientSpecifications = new ColorBlend(3);
+            // Define positions of gradient colors, use intesity to adjust the middle color to
+            // show more mask or less mask
+            GradientSpecifications.Positions = new float[3] { 0, fIntensity, 1 };
+            // Define gradient colors and their alpha values, adjust alpha of gradient colors to match intensity
+            GradientSpecifications.Colors = new System.Drawing.Color[3]
+            {
+            System.Drawing.Color.FromArgb(0, System.Drawing.Color.White),
+            System.Drawing.Color.FromArgb(HeatPoint.Intensity, System.Drawing.Color.Black),
+            System.Drawing.Color.FromArgb(HeatPoint.Intensity, System.Drawing.Color.Black)
+            };
+            // Pass off color blend to PathGradientBrush to instruct it how to generate the gradient
+            GradientShaper.InterpolationColors = GradientSpecifications;
+            // Draw polygon (circle) using our point array and gradient brush
+            Canvas.FillPolygon(GradientShaper, CircumferencePointsArray);
+        }
+        private double ConvertDegreesToRadians(double degrees)
+        {
+            double radians = (Math.PI / 180) * degrees;
+            return (radians);
+        }
+
+        public struct HeatPoint
+        {
+            public int X;
+            public int Y;
+            public byte Intensity;
+            public HeatPoint(int iX, int iY, byte bIntensity)
+            {
+                X = iX;
+                Y = iY;
+                Intensity = bIntensity;
+            }
+        }
+    }
+}
